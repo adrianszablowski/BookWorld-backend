668bf7cf5ccf0ff85ad339d716904f0c70c983f0
adrianszablowski <adrian.szablowski.kontakt@gmail.com>
2025-02-05 20:53:53 +0100

    feat: install swagger and graphql

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('BooksWorld')
+    .setDescription('Books API')
+    .setVersion('1.0')
+    .addTag('books')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
