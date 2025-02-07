bdb228bdb9a19e9fd871823a97e04d39db4647d3
adrianszablowski <adrian.szablowski.kontakt@gmail.com>
2025-02-07 22:34:29 +0100

    feat: add apiProperty to Dto

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,6 +1,14 @@
-import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateBookDto {
+  @ApiProperty({
+    example: 'Harry Potter',
+    description: 'Book title',
+    nullable: false,
+    type: String,
+  })
   @IsString()
+  @IsNotEmpty()
   readonly title: string;
 }
